534926ea66e14d8cd6fb9e7b5edee18a6c945395
Dabskiiee <2022310597@dhvsu.edu.ph>
2024-09-25 00:59:17 +0800

    spidran vro
    kkkk

--- config/settings-configuration.php
+++ config/settings-configuration.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    include_once __DIR__ .'/../database/dbconnection.php';
 
     ini_set('display_errors',1);
     ini_set('display_startup_errors',1);
@@ -13,4 +14,34 @@
         $csrf_token = $_SESSION['csrf_token'];
     }
 
-?>
+    class SystemConfig {
+        private $conn;
+        private $smtp_email;
+        private $smtp_password;
+
+        public function __construct() 
+        { 
+            $database = new Database();
+            $db = $database->dbconnection();
+            $this->conn =$db;
+            
+            $stmt = $this->runQuery("SELECT  * FROM email_config");
+            $stmt ->execute();
+            $email_config =$stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $this->smtp_email = $email_config['email'];
+            $this->smtp_password = $email_config['password'];
+        }
+
+        public function getSmtpEmail(){
+            return $this->smtp_email;   
+        }    
+
+        public function getSmtpPassword(){
+            return $this->smtp_password;    
+        }
+    public function runQuery($sql){
+    $stmt = $this->conn->prepare($sql);
+    return $stmt;  
+    }
+}
--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
-    session_start();
-
-    ini_set('display_errors',1);
-    ini_set('display_startup_errors',1);
-    error_reporting(E_ALL);
-
-    if (empty($_SESSION['csrf_token'])){
-        $csrf_token = bin2hex(random_bytes(32));
-        $_SESSION['csrf_token'] = $csrf_token;
-    }
-    else{
-        $csrf_token = $_SESSION['csrf_token'];
-    }
+include_once 'config/settings-configuration.php';
 ?>
 
 <!DOCTYPE html>
